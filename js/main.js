1c2f12681a3ab8c12139ad7df51e6bfaeae48603
Mario Rivera <mario_rivera.olivo@outlook.es>
2019-07-18 23:18:08 -0600

    Main page on app

---
+++ js/main.js
@@ -0,0 +1,118 @@
+window.onload = function(){
+    // BURGER
+    const burger = document.querySelector('.burger');
+    const bars = document.querySelector('.burger .fa-bars');
+    const ex = document.querySelector('.burger .fa-times');
+    const linksMobile = document.querySelector("nav .linksMobile");
+
+    if(burger){
+        burger.addEventListener('click', function(){
+            if(ex.style.display == '' || ex.style.display == 'none'){
+                ex.style.display = 'inline';
+                bars.style.display = 'none';
+                linksMobile.style.display = 'flex';
+            }else{
+                ex.style.display = 'none';
+                bars.style.display = 'inline';
+                linksMobile.style.display = 'none';
+            }
+        });
+    }
+
+
+    // SLIDER
+    let images = ['img1.jpg','img2.jpg','img3.jpg','img4.jpg'];
+        const next = document.querySelector(".slider .next");
+        const prev = document.querySelector(".slider .prev");
+        let cont = -1;
+        let time = 5000;
+        
+        const slider = document.querySelector(".slider");
+        const sliderimg = document.querySelector(".slider .slider-img");
+        const sliderTitle = document.querySelector(".slider-info h3");
+        const sliderContent = document.querySelector(".slider-info p");
+        const sliderLink = document.querySelector(".slider-info a");
+
+        function Noticia(){
+            let img;
+            let titulo;
+            let contenido;
+            let link;
+            this.Noticia = function(i, t, c, l){
+                img = i;
+                titulo = t;
+                contenido = c;
+                link = l;
+            }
+            Object.defineProperty(this, 'img', {
+                get : function(){return img;},
+                set : function(value){img = value;}
+            });
+            Object.defineProperty(this, 'titulo', {
+                get : function(){return titulo;},
+                set : function(value){titulo = value;}
+            });
+            Object.defineProperty(this, 'contenido', {
+                get : function(){return contenido;},
+                set : function(value){contenido = value;}
+            });
+            Object.defineProperty(this, 'link', {
+                get : function(){return link;},
+                set : function(value){link = value;}
+            });
+        }
+
+        let noticia1 = new Noticia();
+        noticia1.Noticia("img1.jpg", "Sólo hoy: ¡Consultas gratis!", "Ven hoy a nuestra clínica, entre 8 y 12 de la mañana, y podrás tener una consulta gratis.", "nothing.com");
+        let noticia2 = new Noticia();
+        noticia2.Noticia("img2.jpg", "Otra noticia", "Pues esta es otra noticia", "nothing.com");
+        let noticia3 = new Noticia();
+        noticia3.Noticia("img3.jpg", "Sólo hoy: ¡Consultas gratis!", "Ven hoy a nuestra clínica, entre 8 y 12 de la mañana, y podrás tener una consulta gratis.", "nothing.com");
+        let noticia4 = new Noticia();
+        noticia4.Noticia("img4.jpg", "Otra noticia", "Pues esta es otra noticia", "nothing.com");
+
+        let noticiasArray = [noticia1, noticia2, noticia3, noticia4];
+
+        function changeNew(contador){
+            slider.style = `background-image: url('../img/${noticiasArray[contador].img}');`;
+            sliderimg.setAttribute("src", "../img/" +  noticiasArray[contador].img);
+            sliderTitle.textContent = noticiasArray[contador].titulo;
+            sliderContent.textContent = noticiasArray[contador].contenido;
+            sliderLink.setAttribute("href", noticiasArray[contador].link);
+        }
+
+        function changeImage(){
+            if(cont < noticiasArray.length - 1){
+                cont++;
+            }else{
+                cont = 0;
+            }
+            changeNew(cont);
+        }
+        
+        next.addEventListener('click', nextImage);
+        function nextImage(){
+            if(cont < images.length - 1){
+                cont++;
+            }else{
+                cont = 0;
+            }
+            changeNew(cont);
+        }
+        
+        prev.addEventListener('click', prevImage);
+        function prevImage(){
+            if(cont > 0){
+                cont--;
+            }else{
+                cont = images.length - 1;
+            }
+            changeNew(cont);
+        }
+        
+        function timeout(){
+            changeImage();
+            setTimeout(timeout, time);
+        }
+        timeout();
+}
--- web/home/principal.jsp
+++ web/home/principal.jsp
@@ -2,6 +2,46 @@
     <title>Principal</title>
 <%@include file="../WEB-INF/jspf/_navbar.jspf" %>
 
-    <h1>Página principal!</h1>
+    <div class="contenedor">
+        <main>
+            <div class="slider-container">
+                <div class="slider">
+                    <div class="prev"><i class="icon icon-circle-left"></i></div>
+                    <div class="next"><i class="icon icon-circle-right"></i></div>
+                    <div class="slider-content">
+                        <img class="slider-img" alt="">
+                        <div class="slider-info">
+                            <h3>Noticia</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio laboriosam facere aperiam beatae nisi aliquam commodi est, possimus necessitatibus tempora quaerat quo odio esse ipsa omnis accusamus rem eius in eligendi error ipsam voluptatem. Commodi, pariatur. Quidem itaque sequi maxime laudantium, accusamus similique nisi? Recusandae hic placeat expedita numquam pariatur.</p>
+                            <a href="" class="vermas vermas-2">Ver más</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <section id="servicios">
+                <div class="vanish">
+                    <div class="sec">
+                        <h2>Servicios</h2>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit repudiandae pariatur distinctio adipisci vero quaerat alias, at exercitationem inventore earum!</p>
+                        <a href="" class="vermas">Ver más <i class="icon icon-circle-with-plus"></i></a>
+                    </div>
+                </div>
+            </section>
+            <section id="citas">
+                <div class="vanish-2">
+                    <h2>Citas</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit repudiandae pariatur distinctio adipisci vero quaerat alias, at exercitationem inventore earum!</p>
+                    <a href="" class="vermas">Ver más <i class="icon icon-circle-with-plus"></i></a>
+                </div>
+            </section>
+            <section id="acercade">
+                <div class="vanish-other">
+                    <h2>Acerca de nosotros</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit repudiandae pariatur distinctio adipisci vero quaerat alias, at exercitationem inventore earum!</p>
+                    <a href="" class="vermas">Ver más <i class="icon icon-circle-with-plus"></i></a>
+                </div>
+            </section>
+        </main>
+    </div>
     
 <%@include file="../WEB-INF/jspf/_footer.jspf" %>
